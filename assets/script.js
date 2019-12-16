77349bdc694114615c0135e9337da5d561a15731
pb-totoro <jonesec2@gmail.com>
2019-12-15 22:02:33 -0500

    Created on click and ajax setup

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,17 @@ $(function () {
     $("#tabs").tabs();
 });
 
-var cityName = $('#cityInput').val().trim();
+$('#cityInput').on("click", function() {
+    event.preventDefault();
+    var cityName = $('#cityInput').val().trim();
+    console.log(cityName)
+    
+    var cityURL = "api.openweathermap.org/data/2.5/weather?q=" + cityName
 
-var cityURL = "api.openweathermap.org/data/2.5/weather?q=" + cityName
+    $.ajax({
+        url: cityURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response)
+    })
+})
